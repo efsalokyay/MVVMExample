d487990c0998af971c23e2d1913fd20cbccd2da2
efsalokyay <35918590+efsalokyay@users.noreply.github.com>
2019-06-02 18:33:33 +0300

    Yazan: Efsal Okyay

---
+++ app/src/main/java/com/example/architectureexample/Note.java
@@ -0,0 +1,44 @@
+package com.example.architectureexample;
+
+import android.arch.persistence.room.ColumnInfo;
+import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.PrimaryKey;
+
+@Entity(tableName = "note_table")
+public class Note {
+
+    @PrimaryKey(autoGenerate = true)
+    private int Id;
+
+    private String title;
+
+    private String description;
+
+    private int priority;
+
+    public Note(String title, String description, int priority) {
+        this.title = title;
+        this.description = description;
+        this.priority = priority;
+    }
+
+    public int getId() {
+        return Id;
+    }
+
+    public void setId(int Id) {
+        this.Id = Id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
+}
